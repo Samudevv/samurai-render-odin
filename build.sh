2b4d9356e1a53f692888b58371b47aecb7cd0eaf
PucklaJ <jonaas.pucher000000@gmail.com>
2023-11-10 22:25:36 +0100

    Disable cairo backend

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo Building C Library samurai-render ...
-xmake config -P lib/samurai-render --backend_opengl=y
+xmake config -P lib/samurai-render --backend_opengl=y --backend_cairo=n
 xmake -P lib/samurai-render
 ln -fs libsamurai-render.a $(pwd)/build/linux/x86_64/release/libsamurai_render.a
 
--- examples/opengl_bounce/build.sh
+++ examples/opengl_bounce/build.sh
@@ -2,6 +2,6 @@
 
 echo Building opengl_bounce example ...
 odin build examples/opengl_bounce \
--extra-linker-flags='-L./build/linux/x86_64/release -lwayland-client -lwayland-cursor -lwayland-egl -lcairo -lEGL' \
+-extra-linker-flags='-L./build/linux/x86_64/release -lwayland-client -lwayland-cursor -lwayland-egl -lEGL' \
 -out:examples/opengl_bounce/opengl_bounce \
 -o:minimal
