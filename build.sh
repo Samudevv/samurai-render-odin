5a0b451fb1042ac2908a7169eda83b368eae0862
PucklaJ <jonaas.pucher000000@gmail.com>
2023-11-10 19:44:52 +0100

    Add opengl backend support

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -e
 
 echo Building C Library samurai-render ...
+xmake config -P lib/samurai-render --backend_opengl=y
 xmake -P lib/samurai-render
 ln -fs libsamurai-render.a $(pwd)/build/linux/x86_64/release/libsamurai_render.a
 
 examples/blank/build.sh
+examples/opengl_bounce/build.sh
--- examples/blank/build.sh
+++ examples/blank/build.sh
@@ -2,6 +2,6 @@
 
 echo Building blank example ...
 odin build examples/blank \
--extra-linker-flags='-L./build/linux/x86_64/release -lwayland-client -lwayland-cursor -lcairo' \
+-extra-linker-flags='-L./build/linux/x86_64/release -lwayland-client -lwayland-cursor -lwayland-egl -lcairo -lEGL' \
 -out:examples/blank/blank \
 -o:minimal
